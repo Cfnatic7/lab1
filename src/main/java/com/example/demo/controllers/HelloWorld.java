a0e4bd6fceddd89f23ca0cf0b8df1d70430cec6c
Cfnatic7 <78828448+Cfnatic7@users.noreply.github.com>
2023-03-04 14:22:27 +0100

    feat: session and cookies

--- src/main/java/com/example/demo/controllers/HelloWorld.java
+++ src/main/java/com/example/demo/controllers/HelloWorld.java
@@ -1,18 +1,26 @@
 package com.example.demo.controllers;
 
 import com.example.demo.dtos.LoginDto;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.ServletConfig;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class HelloWorld {
 
+    public static final String LOGGED_IN = "loggedIn";
+    public static final String SESSION_RESULT = "session-result";
     private int integer = 1;
 
     @GetMapping("/index")
@@ -52,25 +60,55 @@ public class HelloWorld {
         return "params";
     }
 
+    @GetMapping("/login-form")
+    public String loginForm(Model model) {
+        model.addAttribute("login", new LoginDto());
+        return "session";
+    }
+
+    @GetMapping("/logout-button")
+    public String logoutButton() {
+        return "logout-button";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.setAttribute(LOGGED_IN, false);
+        return SESSION_RESULT;
+    }
+
     @PostMapping("/session")
     public String session(Model model, HttpSession session, @ModelAttribute("login") LoginDto login) {
         if (login.getPassword().equals("test") && login.getUserName().equals("test")) {
-            model.addAttribute("loggedIn", "true");
-            session.setAttribute("loggedIn", true);
+            model.addAttribute(LOGGED_IN, true);
+            session.setAttribute(LOGGED_IN, true);
         }
         else {
-            model.addAttribute("loggedIn", "false");
-            session.setAttribute("loggedIn", false);
+            model.addAttribute(LOGGED_IN, false);
+            session.setAttribute(LOGGED_IN, false);
         }
-        return "session-result";
+        return SESSION_RESULT;
     }
 
     @GetMapping("/session-result")
     public String getSessionResult(Model model, HttpSession session) {
-        model.addAttribute("loggedIn",
-                session.getAttribute("loggedIn") != null ?
-                        session.getAttribute("loggedIn") : false);
-        return "session-result";
+        model.addAttribute(LOGGED_IN,
+                session.getAttribute(LOGGED_IN) != null ?
+                        session.getAttribute(LOGGED_IN) : false);
+        return SESSION_RESULT;
+    }
+
+    @GetMapping("/count-cookie")
+    public ResponseEntity<?> countCookie(Model model, @CookieValue(name = "count", defaultValue = "0") String count) {
+        ResponseCookie cookie = ResponseCookie.from("count", String.valueOf(Integer.parseInt(count) + 1))
+                .httpOnly(true)
+                .maxAge(60)
+                        .build();
+
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .body(cookie.getValue());
     }
 
 
